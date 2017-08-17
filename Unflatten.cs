6d8cf9ad69110f92ec682a528eab7bf034029cb1
Victor Novais <vicnovais@gmail.com>
2017-08-17 09:59:38 -0300

    Namespace

--- Unflatten.cs
+++ Unflatten.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace Juritis.LegalDesk.WebUtil.Extensions
+namespace Extensions
 {
     public class Unflatten
     {
